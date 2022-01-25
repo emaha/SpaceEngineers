b3aeed0cc63f9ec8fd161299342748f3d6df0aca
abarkin <a.m.barkin@gmail.com>
2022-01-25 14:44:53 +0300

    Added Sentis shop

---
+++ SpaceEngineers/SentiumShopManager.cs
@@ -0,0 +1,297 @@
+﻿
+
+using Sandbox.ModAPI.Ingame;
+using System.Text;
+using VRage.Game.ModAPI.Ingame;
+
+namespace SpaceEngineersSentiumShopManager
+{
+    // Скрипт для магаза 
+    public sealed class Program : MyGridProgram
+    {
+        IMyTextPanel LCDInput;
+        IMyTextPanel LCDOutput;
+        IMyTextPanel LCDController;
+        IMyTextPanel LCDError;
+
+        IMyCargoContainer Input;
+        IMyCargoContainer Output;
+        IMyCargoContainer Vault;
+
+        IMyInventory inputInventory;
+        IMyInventory outputInventory;
+        IMyInventory vaultInventory;
+
+        IMyTerminalBlock mySoundBlock;
+
+        string errorText = string.Empty;
+        string tradeLog = "Trade log:\n";
+        const string sentiumName = "SP_Gift";
+        int currentIndex = 0;
+
+        const int T1price = 150;
+        const int T2price = 50;
+        const int T3price = 5;
+
+        int tick = 0;
+
+        Dictionary<string, int> prices = new Dictionary<string, int>
+        {
+            // T3
+            //{"Gee", T3price },
+            //{"Irr", T3price },
+            //{"Tii", T3price },
+            //{"Znn", T3price },
+
+            // T2
+            {"Aluminum", T2price },
+            {"Copper", T2price },
+            {"Scandium", T2price },
+            {"Molybdenum", T2price },
+            {"Platinum", T2price },
+
+            // Vanilla
+            {"Uranium", 50 },
+            {"Iron", T1price },
+            {"Gold", T1price },
+            {"Cobalt", T1price },
+            {"Silicon", T1price },
+            {"Magnesium", T1price },
+            {"Nickel", T1price },
+            {"Silver", T1price },
+        };
+        Dictionary<string, string> locale = new Dictionary<string, string>
+        {
+            // T3
+            //{"Gee", "Германий" },
+            //{"Irr", "Иридий" },
+            //{"Tii", "Титан" },
+            //{"Znn", "Цинк" },
+
+            // T2
+            {"Aluminum", "Алюминий" },
+            {"Copper", "Медь" },
+            {"Scandium", "Скандий"},
+            {"Molybdenum", "Молибден"},
+            {"Platinum", "Платина" },
+
+            // Vanilla
+            {"Uranium", "Уран" },
+            {"Iron", "Железо" },
+            {"Gold", "Золото" },
+            {"Cobalt", "Кобальт" },
+            {"Silicon", "Кремний" },
+            {"Magnesium", "Магний"},
+            {"Nickel", "Никель"},
+            {"Silver", "Серебро" }
+        };
+
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+
+            mySoundBlock = GridTerminalSystem.GetBlockWithName("Alarm") as IMyTerminalBlock;
+
+            LCDInput = GridTerminalSystem.GetBlockWithName("LcdInput") as IMyTextPanel;
+            LCDOutput = GridTerminalSystem.GetBlockWithName("LcdOutput") as IMyTextPanel;
+            LCDController = GridTerminalSystem.GetBlockWithName("LcdController") as IMyTextPanel;
+            LCDError = GridTerminalSystem.GetBlockWithName("LcdError") as IMyTextPanel;
+
+            List<IMyTerminalBlock> inOutContainerList = new List<IMyTerminalBlock>();
+            var containers = GridTerminalSystem.GetBlockGroupWithName("InOut");
+            containers.GetBlocks(inOutContainerList);
+            var first = inOutContainerList.First() as IMyCargoContainer;
+            var last = inOutContainerList.Last() as IMyCargoContainer;
+
+            if(first.GetInventory().MaxVolume > last.GetInventory().MaxVolume)
+            {
+                Input = last;
+                Output = first;
+            }
+            else
+            {
+                Input = first;
+                Output = last;
+            }
+
+
+            //Input = GridTerminalSystem.GetBlockWithName("ContainerInput") as IMyCargoContainer;
+            //Output = GridTerminalSystem.GetBlockWithName("ContainerOutput") as IMyCargoContainer;
+            Vault = GridTerminalSystem.GetBlockWithName("ContainerVault") as IMyCargoContainer;
+
+            Echo($"Containers ready: {Input != null && Output != null && Vault != null}");
+
+            inputInventory = Input.GetInventory(0);
+            outputInventory = Output.GetInventory(0);
+            vaultInventory = Vault.GetInventory(0);
+        }
+
+        public void Main(string argument)
+        {
+            LCDController.WriteText(GetMenuText(currentIndex));
+
+            switch (argument)
+            {
+                case "trade":
+                    Trade();
+                    break;
+
+                case "up":
+                    errorText = "";
+                    if(currentIndex>0) currentIndex--;
+                    break;
+
+                case "down":
+                    errorText = "";
+                    if (currentIndex < prices.Count-1) currentIndex++;
+                    break;
+            }
+
+            ShowErrorLog();
+            ShowInputInfo();
+        }
+
+        private void ShowErrorLog()
+        {
+            LCDError.WriteText($"Ошибки:\n\n\n{errorText}");
+        }
+
+        private void ShowInputInfo()
+        {
+            var sentium = FindItem(inputInventory, sentiumName);
+
+            var inText = $"\n\n\nВы отдаете:\n" +
+                $"Сентиум x {(sentium.HasValue ? sentium.Value.Amount : 0)}\n";
+            var outText = $"\n\n\nВы получите:\n" +
+                $"{GetLocalIngotName(GetCurrentIngotName())} x {(sentium.HasValue ? sentium.Value.Amount * GetCurrentIngotRatio() : 0)}";
+
+            LCDInput.WriteText($"{inText}");
+            LCDOutput.WriteText($"{outText}");
+
+            if(tick++ > 1)  mySoundBlock.GetActionWithName("StopSound").Apply(mySoundBlock);
+        }
+
+        private void Trade()
+        {
+            List<MyInventoryItem> inputItems = new List<MyInventoryItem>();
+            inputInventory.GetItems(inputItems);
+
+            errorText = string.Empty;
+
+            foreach (var it in inputItems)
+            {
+                var amount = it.Amount;
+
+                var inputItem = FindItem(inputInventory, sentiumName);
+                var returnItem = FindItem(vaultInventory, GetCurrentIngotName());
+                if(inputItem.HasValue)
+                {
+                    VRage.MyFixedPoint outputVolume = inputItem.Value.Amount * GetCurrentIngotRatio();
+
+                    if (!returnItem.HasValue || returnItem.Value.Amount < outputVolume.ToIntSafe())
+                    {
+                        errorText += "Извините.\n" +
+                            "Не достаточно\n" +
+                            "ресурсов для обмена\n" +
+                            "на станции.\n\n" +
+                            "Попробуйте выбрать\n" +
+                            "меньшее количество.";
+                        Alarm();
+                        return;
+                    }
+
+                    if(!CheckIfEnoughSpaceInOutput(returnItem.Value, outputVolume))
+                    {
+                        Alarm();
+                        return;
+                    }
+
+                    inputInventory.TransferItemTo(vaultInventory, inputItem.Value); 
+                    outputInventory.TransferItemFrom(vaultInventory, returnItem.Value, outputVolume);
+
+                    tradeLog += $"{amount} {sentiumName} -> {GetCurrentIngotName()} {outputVolume}\n";
+                }
+                
+            }
+            
+        }
+
+        private void Alarm()
+        {
+            tick = 0;
+            mySoundBlock.GetActionWithName("PlaySound").Apply(mySoundBlock);
+        }
+
+        private MyInventoryItem? FindItem(IMyInventory inventory, string ingotName)
+        {
+            List<MyInventoryItem> items = new List<MyInventoryItem>();
+            inventory.GetItems(items);
+            foreach (var item in items)
+            {
+                if (item.ToString().Contains(ingotName))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        private string FormatItem(MyInventoryItem item)
+        {
+            var name = item.ToString();
+            return $"{name.ToString().Substring(name.IndexOf('/') + 1)}";
+        }
+
+        private string GetMenuText(int index)
+        {
+            var sb = new StringBuilder();
+            for(int i = 0; i < prices.Count;i++)
+            {
+                if(i == index)
+                {
+                    sb.Append($">>>>> {GetLocalIngotName(prices.ElementAt(i).Key)} 1:{prices.ElementAt(i).Value} <<<<<\n");
+                }
+                else
+                {
+                    sb.Append($"            {GetLocalIngotName(prices.ElementAt(i).Key)} 1:{prices.ElementAt(i).Value}\n");
+                }
+                
+            }
+
+            return sb.ToString();
+        }
+
+        private bool CheckIfEnoughSpaceInOutput(MyInventoryItem item, VRage.MyFixedPoint amount)
+        {
+            var typeVolume = item.Type.GetItemInfo().Volume;
+            var volume = typeVolume * amount.ToIntSafe() * 1000;
+            var freeVolume = (outputInventory.MaxVolume - outputInventory.CurrentVolume) * 1000;
+
+            var isEnough = freeVolume.ToIntSafe() >= volume;
+            if (!isEnough)
+            {
+                errorText += "Не достаточно места \nв выходном контейнере\n\n" +
+                    $"Нужно: {volume}л\n\n" +
+                    $"Свободно: {freeVolume.ToIntSafe()}л\n";
+            }
+
+            return isEnough;
+        }
+
+        private string GetCurrentIngotName()
+        {
+            return prices.ElementAt(currentIndex).Key;
+        }
+
+        private int GetCurrentIngotRatio()
+        {
+            return prices.ElementAt(currentIndex).Value;
+        }
+
+        private string GetLocalIngotName(string name)
+        {
+            return locale[name];
+        }
+
+    }
+}
---
+++ SpaceEngineers/SwarmController.cs
@@ -0,0 +1,105 @@
+﻿using Sandbox.ModAPI.Ingame;
+using VRageMath;
+
+namespace SpaceEngineersSwarmController
+{
+    // Управление стаей ботов
+    public sealed class Program : MyGridProgram
+    {
+        enum State
+        {
+            FOLLOW,
+            STOP
+        }
+
+        State state = State.STOP;
+        
+        IMyRemoteControl RemCon;
+        IMyTextPanel LCD;
+        List<IMyBroadcastListener> listeners = new List<IMyBroadcastListener>();
+        double distance = 50;
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+
+            LCD = GridTerminalSystem.GetBlockWithName("LCD") as IMyTextPanel;
+            RemCon = GridTerminalSystem.GetBlockWithName("RemCon") as IMyRemoteControl;
+
+            IGC.RegisterBroadcastListener("Ch2");
+            IGC.GetBroadcastListeners(listeners);
+        }
+
+        public void Main(string argument)
+        {
+            if(argument == "follow")
+            {
+                state = State.FOLLOW;
+            }
+
+            if (argument == "stop")
+            {
+                state = State.STOP;
+                IGC.SendBroadcastMessage("Ch1", $"stop", TransmissionDistance.AntennaRelay);
+            }
+
+            switch (state)
+            {
+                case State.STOP:
+                    LCD.WriteText($"STOP");
+
+                    break;
+                case State.FOLLOW:
+                    var myPos = RemCon.GetPosition();
+                    var dir = RemCon.WorldMatrix.GetOrientation().GetDirectionVector(Base6Directions.Direction.Forward);
+                    Vector3D worldDirection = Vector3D.TransformNormal(dir, RemCon.WorldMatrix);
+                    var gPos = myPos + dir * distance;
+                    var pos = gPos;
+
+                    var msg = $"follow;{pos.X};{pos.Y};{pos.Z};" +
+                        $"{pos.X};{pos.Y};{pos.Z};";
+
+                    IGC.SendBroadcastMessage("Ch1", $"{msg}", TransmissionDistance.AntennaRelay);
+                    LCD.WriteText($"myPos:{myPos}\n");
+                    LCD.WriteText($"pos:{gPos}\n",true);
+                    break;
+            }
+                
+            if (listeners.Any() && listeners.FirstOrDefault().HasPendingMessage)
+            {
+                MyIGCMessage message = new MyIGCMessage();
+                message = listeners[0].AcceptMessage();
+                string messagetext = message.Data.ToString();
+                string messagetag = message.Tag;
+                long sender = message.Source;
+
+                //Do something with the information!
+                Echo("Message received with tag" + messagetag + "\n\r");
+                Echo("from address " + sender.ToString() + ": \n\r");
+                Echo(messagetext);
+
+                
+                var msg = messagetext.Split(';');
+                var cmd = msg.FirstOrDefault();
+                LCD.WriteText($"{messagetext}\n", true);
+
+                switch (cmd)
+                {
+                    case "follow":
+                        break;
+                    case "point":
+                        break;
+                }
+
+                
+                //if (msg.FirstOrDefault() == botName)
+                //{
+                //    currentTargetPos = new Vector3D(Convert.ToDouble(msg[1]), Convert.ToDouble(msg[2]), Convert.ToDouble(msg[3]));
+                //    state = State.MOVING;   
+                //}
+                
+            }
+
+        }
+
+    }
+}
